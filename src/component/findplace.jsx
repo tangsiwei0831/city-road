cb90135998013155cced31fab5cf0128206fd177
SiweiTang <siweitang@hotmail.com>
2024-07-12 15:33:29 -0400

    add some element and change tab name

--- src/component/findplace.jsx
+++ src/component/findplace.jsx
@@ -2,10 +2,16 @@ import '../styles/findplace.css'
 
 function FindPlace() {
     return (
-      <div>
-        <h3 className='site-header'>city roads</h3>
-        <p className='description'>This website renders every single road within a city</p>
-      </div>
+    <div className='find-place'>
+        <div>
+            <h3 className='site-header'>city roads</h3>
+            <p className='description'>This website renders every single road within a city</p>
+        </div>
+        <form className='search-box'>
+            <input type='text' placeholder='Ener a city name to start'></input>
+            <a className='search-submit' type='submit'>Find City Bounds</a>
+        </form>
+    </div>
     )
   }
   
